477bffacb06354bdc897429c1142f9f99ff0a509
Joonas Loppi <joonas@joonas.fi>
2018-10-16 16:44:28 +0300

    infrastructureascode: introduce namespaces

--- cmd/james/infrastructureascode.go
+++ cmd/james/infrastructureascode.go
@@ -1,43 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 func iacEntry() *cobra.Command {
 	return &cobra.Command{
-		Use:   "iac",
+		Use:   "iac [namespace]",
 		Short: "Enters infrastructureascode container",
-		Args:  cobra.NoArgs,
+		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			namespace := args[0]
+
 			jamesfile, err := readJamesfile()
 			reactToError(err)
 
 			cwd, err := os.Getwd()
 			reactToError(err)
 
-			fmt.Printf("Entering infrastructure-as-code container. Press ctrl+c to exit\n")
+			pathToNamespaceFile := func(filename string) string {
+				return cwd + "/" + namespace + "/" + filename
+			}
 
-			runIac := exec.Command(
+			reactToError(touch(pathToNamespaceFile("terraform.tfstate")))
+			reactToError(touch(pathToNamespaceFile("terraform.tfstate.backup")))
+
+			dockerArgs := []string{
 				"docker",
 				"run",
 				"--rm",
 				"-it",
 				// for Packer
-				"-e", "DIGITALOCEAN_API_TOKEN="+jamesfile.DigitalOceanApiToken,
+				"-e", "DIGITALOCEAN_API_TOKEN=" + jamesfile.Credentials.DigitalOcean.Password,
 				// for Terraform (yes, different key for same thing)
-				"-e", "DIGITALOCEAN_TOKEN="+jamesfile.DigitalOceanApiToken,
-				"-e", "CLOUDFLARE_EMAIL="+jamesfile.CloudflareEmail,
-				"-e", "CLOUDFLARE_TOKEN="+jamesfile.CloudflareToken,
-				"-v", cwd+"/nodes.tf:/work/nodes.tf",
-				"-v", cwd+"/services.tf:/work/services.tf",
-				"-v", cwd+"/terraform.tfstate:/work/terraform.tfstate",
-				"-v", cwd+"/terraform.tfstate.backup:/work/terraform.tfstate.backup",
-				jamesfile.InfrastructureAsCodeImageVersion)
+				"-e", "DIGITALOCEAN_TOKEN=" + jamesfile.Credentials.DigitalOcean.Password,
+				"-e", "CLOUDFLARE_EMAIL=" + jamesfile.Credentials.Cloudflare.Username,
+				"-e", "CLOUDFLARE_TOKEN=" + jamesfile.Credentials.Cloudflare.Password,
+				"-e", "AWS_ACCESS_KEY_ID=" + jamesfile.Credentials.AWS.Username,
+				"-e", "AWS_SECRET_ACCESS_KEY=" + jamesfile.Credentials.AWS.Password,
+				"-v", cwd + "/scripts:/work/scripts",
+				"-v", pathToNamespaceFile("terraform.tfstate") + ":/work/terraform.tfstate",
+				"-v", pathToNamespaceFile("terraform.tfstate.backup") + ":/work/terraform.tfstate.backup",
+			}
 
+			tfFilesFromNamespace, err := filepath.Glob(namespace + "/*.tf")
+			reactToError(err)
+
+			if len(tfFilesFromNamespace) == 0 {
+				reactToError(errors.New("no *.tf files found"))
+			}
+
+			for _, tfFile := range tfFilesFromNamespace {
+				// remove the path to the dir and keep only filename
+				filename := filepath.Base(tfFile)
+
+				mapping := pathToNamespaceFile(filename) + ":/work/" + filename
+
+				dockerArgs = append(dockerArgs, "-v", mapping)
+			}
+
+			dockerArgs = append(dockerArgs, jamesfile.InfrastructureAsCodeImageVersion)
+
+			fmt.Printf("Entering infrastructure-as-code container. Press ctrl+c to exit\n")
+
+			runIac := exec.Command(dockerArgs[0], dockerArgs[1:]...)
 			runIac.Stdin = os.Stdin
 			runIac.Stdout = os.Stdout
 			runIac.Stderr = os.Stderr
@@ -45,4 +76,22 @@ func iacEntry() *cobra.Command {
 			reactToError(runIac.Run())
 		},
 	}
+}
+
+// creates an empty file if it does not exist
+func touch(path string) error {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			emptyFile, err := os.Create(path)
+			if err != nil {
+				return err
+			}
+
+			return emptyFile.Close()
+		}
+
+		return err // an actual error
+	}
+
+	return nil
 }
